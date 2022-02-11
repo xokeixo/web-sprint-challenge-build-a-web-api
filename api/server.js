dc4903c942032b773ce3de4094ee72cf9a52c8f1
Markeisha <markeisha94@gmail.com>
2022-02-11 11:59:27 -0600

    set up server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const server = express();
+const cors = require('cors');
+const morgan = require('morgan');
 
 // Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+server.use(express.json());
+server.use(morgan('tiny'));
+server.use(cors());
+server.use('/api/actions', actionsRouter);
+server.use('/api/projects', projectsRouter);
+
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
+
+
+server.get('/', (req, res) => {
+    res.send(`<h3>Server is currently working!</h3>`)
+})
 
 module.exports = server;
